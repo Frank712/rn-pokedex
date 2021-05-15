cb7955b01d5ac504cf6d02764c22f28b09938ba1
Francisco Lino <franksuriel_7@hotmail.com>
2021-05-14 22:53:43 -0500

    inital commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text>
+        Welcome to Pokedex      
+      </Text>
+    </View>
+  )
+}
+
+export default App
